1d57f952e160075c0246c68020e42c5bdcc93894
ekunish <ekunish@gmail.com>
2021-02-08 19:30:53 +0900

    web audio API trial

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -4,4 +4,6 @@ interface Window {
     test: (message: string) => void; //（2）test関数の型定義
     openFile: () => Promise<{ filename: string; content: string } | null>; //（2）openFile関数を定義
   };
+
+  webkitAudioContext: typeof AudioContext;
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,11 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TopPage from './components/pages/TopPage';
 
 function App() {
-  //（1）filename, dataをステート定義
-  const [filename, setFilename] = useState("");
-  const [data, setData] = useState({});
-
-  //（2）ダイアログを開く、ボタンのコールバックを定義
-  const handleDialog = useCallback(() => {
-    //（2-1）window.ipc.openFile関数を呼び出して Promise の処理
-    window.ipc.openFile().then((res) => {
-      //（2-2）戻り値がnullでなければ
-      if (res) {
-        // （2-3）setFilenameとsetDataでステートを更新する
-        setFilename(res.filename);
-        setData(JSON.parse(res.content));
-      }
-    });
-  }, []);
-
-  useEffect(() => {
-    data && console.log(data)
-  }, [data])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-
-        <button onClick={() => window.ipc.test("Hello, Electron Main World.")}>
-          ipcテストメッセージ送信
-        </button>
-        <div>
-          <span>{filename}</span>
-          <button onClick={handleDialog}>JSON Open</button>
-        </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div >
+    <TopPage />
   );
 }
 
